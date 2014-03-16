fdbff2de7b99ab8f5f4461a3e5eae97b978bef6a
Paul Tyng <paul@paultyng.net>
2014-03-16 16:07:27 -0400

    wip

--- core/db/migrate/20140313022946_remove_countries_and_states.rb
+++ core/db/migrate/20140313022946_remove_countries_and_states.rb
@@ -14,25 +14,31 @@ class RemoveCountriesAndStates < ActiveRecord::Migration
         set spree_addresses.country_code = spree_countries.iso
           , spree_addresses.region_code = spree_states.abbr
         where (spree_addresses.country_id is null or spree_addresses.country_id = spree_countries.id)
-          and (spree_addresses.state_id is null or spree_addresses.state_id = spree_states.id);
+          and (spree_addresses.state_id is null or spree_addresses.state_id = spree_states.id)
+      }
 
+      execute %{
         update spree_stock_locations, spree_countries, spree_states
         set spree_stock_locations.country_code = spree_countries.iso
           , spree_stock_locations.region_code = spree_states.abbr
         where (spree_stock_locations.country_id is null or spree_stock_locations.country_id = spree_countries.id)
-          and (spree_stock_locations.state_id is null or spree_stock_locations.state_id = spree_states.id);
+          and (spree_stock_locations.state_id is null or spree_stock_locations.state_id = spree_states.id)
+      }
 
+      execute %{
         update spree_zone_members, spree_countries, spree_states
         set spree_zone_members.country_code = spree_countries.iso
         where spree_zone_members.zoneable_type = 'Spree::Country'
-          and spree_zone_members.zoneable_id = spree_countries.id;
+          and spree_zone_members.zoneable_id = spree_countries.id
+      }
 
+      execute %{
         update spree_zone_members, spree_countries, spree_states
         set spree_zone_members.country_code = spree_countries.iso
           , spree_zone_members.region_code = spree_states.abbr
         where spree_zone_members.zoneable_type = 'Spree::State'
           and spree_zone_members.zoneable_id = spree_states.id
-          and spree_states.country_id = spree_countries.id;
+          and spree_states.country_id = spree_countries.id
       }
     else
       execute %{
